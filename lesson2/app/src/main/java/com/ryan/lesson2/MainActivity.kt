d07612b7d7064f98737651ef37d58ee8b9683b6d
guanyu.chen <guanyu.chen@flamingo-inc.com>
2020-04-16 17:35:25 +0800

    add dice image

--- lesson2/app/src/main/java/com/ryan/lesson2/MainActivity.kt
+++ lesson2/app/src/main/java/com/ryan/lesson2/MainActivity.kt
@@ -2,20 +2,20 @@ package com.ryan.lesson2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.core.text.isDigitsOnly
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var resultText: TextView;
+    private lateinit var resultText: TextView
+    private lateinit var diceImage: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         resultText = findViewById(R.id.result_text)
+        diceImage = findViewById(R.id.dice_image)
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
@@ -24,14 +24,15 @@ class MainActivity : AppCompatActivity() {
         countUpButton.setOnClickListener { countUp() }
 
         val resetButton: Button = findViewById(R.id.reset_button)
-        resetButton.setOnClickListener { resultText.text = "0" }
+        resetButton.setOnClickListener { reset() }
     }
 
     private fun rollDice() {
         val randomInt = (1..6).random()
         resultText.text = randomInt.toString()
-//        resultText.text = "Dice Rolled!"
-//        Toast.makeText(this, "button clicked", Toast.LENGTH_LONG).show()
+
+
+        diceImage.setImageResource(getDiceDrawable(randomInt))
     }
 
     private fun countUp() {
@@ -42,6 +43,25 @@ class MainActivity : AppCompatActivity() {
             } else {
                 "6"
             }
+
+            diceImage.setImageResource(getDiceDrawable(resultInt + 1))
+        }
+    }
+
+    private fun reset() {
+        resultText.text = "0"
+
+        diceImage.setImageResource(R.drawable.empty_dice)
+    }
+
+    private fun getDiceDrawable(num: Int): Int {
+        return when (num) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
     }
 }
